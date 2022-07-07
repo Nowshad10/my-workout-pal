f462157bf924d76ed62cf57145d63b13f459dbe7
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-07-07 17:51:52 +0100

    established connection to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}.`)
-});
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Connected to database & listening on port ${port}.`)
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    });
--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -7,18 +7,18 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     res.json({msg: 'GET single workout'})
-})
+});
 
 router.post('/', (req, res) => {
     res.json({msg: 'POST a new workout'})
-})
+});
 
 router.delete('/:id', (req, res) => {
     res.json({msg: 'DELETE a workout'})
-})
+});
 
 router.patch('/:id', (req, res) => {
     res.json({msg: 'UPDATE a workout'})
-})
+});
 
 module.exports = router;
