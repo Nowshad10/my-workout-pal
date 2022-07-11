329859ebbbdc0ad0b85e92d9386ac0c9c7bdb790
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-07-11 18:26:03 +0100

    added logo to h1

--- client/src/layout/Navbar/index.js
+++ client/src/layout/Navbar/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import { Icon } from '@iconify/react';
 
 const Navbar = () => {
   return (
     <header>
         <div className='container'>
             <Link to='/'>
-                <h1 id='main-title'>My Workout Pal</h1>
+                <h1 id='main-title'>My Workout Pal <Icon icon="mdi:dumbbell" inline={true}/></h1>
             </Link>
         </div>
     </header>
