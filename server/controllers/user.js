edaf2674b71ed2fdc3508c0353ef56e686b6345c
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-07-26 22:34:45 +0100

    create and send back jwt after signing up

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,16 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+function createToken(_id) {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1d' });
+};
 
 async function register(req, res) {
     const { username, email, password } = req.body;
     try {
         const user = await User.register(username, email, password);
-        res.status(200).json({username, user});
+        const token = createToken(user._id);
+        res.status(200).json({username, token});
     } catch (error) {
         res.status(400).json({error: error.message});
     };
