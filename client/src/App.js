b21d1331b7a85bd133557bd499ced9947a402e60
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-08-17 17:11:58 +0100

    users that are not logged in can no longer see home page - redirects them to login/signup pages. Logged in users can not see sign up/login pages whilst they are logged in.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <Navbar />
       <div className='pages'>
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
+          <Route path='/' element={user ? <Home /> : <Navigate to='/login' />}/>
+          <Route path='/login' element={!user ? <Login/> : <Navigate to='/'/>}/>
+          <Route path='/register' element={!user ? <Register/> : <Navigate to='/'/>}/>
         </Routes>
       </div>
     </div>
